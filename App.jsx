a6b78c4bb0bc2fbb37fe95bcbcb2d3f810d695ad
mohitppatil2005-afk <mohitppatil2005@gmail.com>
2025-09-29 22:25:28 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,4 @@
+// https://github.com/mohitppatil2005-afk/FloatChat-The-OOPeratives/blob/911a87adefa58f7daba9b873accabb0fbaa8fdb4/FloatChat.jsx
+// (See previous answer for full App.jsx code: paste that entire code block here)
+import React, { useState, useEffect, useCallback, useMemo } from 'react';
+// ... (rest of the App.jsx code from previous answer)
